eb67da8cf27c2f0371427cbe254a2c0f2193f6da
ROHIT-994 <pasumarthirohith@gmail.com>
2020-07-19 10:16:41 +0530

    my git task

---
+++ mytask/src/main/java/git/mytask/App.java
@@ -0,0 +1,13 @@
+package git.mytask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
